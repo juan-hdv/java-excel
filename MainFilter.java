42a9d347e0ae794218d5b0a8ec5111b9a0a86254
Juan Gabriel MEJIA <juanhdv@gmail.com>
2023-05-03 18:01:19 -0500

    Change module

--- MainFilter.java
+++ MainFilter.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
-import com.indracompany.general.Log;
+import com.jgm.general.Log;
 
 import excelfilter.ExcelFilter;
 
